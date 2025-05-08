33b64fbba7250329569e62d24bf0647221ffdb77
Yousuf007 <yousufjobaer@gmail.com>
2025-05-08 19:25:19 +0600

    added Blog in README file

--- Solutions/index.ts
+++ Solutions/index.ts
@@ -1,16 +1,13 @@
-// problem -->1
 function formatString(input: string, toUpper?: boolean): string {
     if (toUpper === false) return input.toLowerCase();
     else {
       return input.toUpperCase();
     }
   }
-  
-  // const result1 =formatString("Hello");
-  // const result2 =formatString("Hello", true);
-  // const result3 =formatString("Hello", false);
-  
-  // problem ---> 2
+
+
+
+
   function filterByRating(
     items: { title: string; rating: number }[]
   ): { title: string; rating: number }[] {
@@ -18,15 +15,9 @@ function formatString(input: string, toUpper?: boolean): string {
     return [...items].filter((item) => item.rating >= 4);
   }
   
-  const books = [
-    { title: "Book A", rating: 4.5 },
-    { title: "Book B", rating: 3.2 },
-    { title: "Book C", rating: 5.0 },
-  ];
+
   
-  //   console.log(filterByRating(books))
-  
-  // problem --> 3
+
   function concatenateArrays<T>(...arrays: T[][]): T[] {
     let result: T[] = [];
   
@@ -35,11 +26,10 @@ function formatString(input: string, toUpper?: boolean): string {
     }
     return result;
   }
-  
-  //console.log(concatenateArrays(["a", "b"], ["c"]));         // Output: ["a", "b", "c"]
-  //console.log(concatenateArrays([1, 2], [3, 4], [5]));       // Output: [1, 2, 3, 4, 5]
-  
-  // problem --> 4
+
+
+
+
   
   class Vehicle {
     private make: string;
@@ -54,7 +44,7 @@ function formatString(input: string, toUpper?: boolean): string {
      return console.log(`Make: ${this.make} Year: ${this.year}`);
     }
   }
-  
+
   class Car extends Vehicle {
     private model: string;
   
@@ -69,8 +59,10 @@ function formatString(input: string, toUpper?: boolean): string {
   }
   const myCar = new Car("Toyota", 2020, "Corolla");
   
-  // problem ---> 5
   
+
+
+
   function processValue(value: string | number): string | number | undefined {
     if (typeof value === "string") {
       return value.length;
@@ -79,16 +71,17 @@ function formatString(input: string, toUpper?: boolean): string {
       return value * 2;
     }
   }
+
   
-  processValue("hello"); // Output: 5
-  processValue(10); // Output: 20
-  
-  // problem ---> 6
+
+
+
+
   interface Product {
     name: string;
     price: number;
   }
-  
+
   function getMostExpensiveProduct(products: Product[]): Product | null {
     if (products.length === 0) {
       return null;
@@ -104,17 +97,36 @@ function formatString(input: string, toUpper?: boolean): string {
     return expensive;
   }
   
-  const products = [
-      { name: "Pen", price: 10 },
-      { name: "Notebook", price: 25 },
-      { name: "Bag", price: 50 }
-    ];
-    
-   getMostExpensiveProduct(products);  // Output: { name: "Bag", price: 50 }
+  
+
+
+
+
+
+
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+  function getDayType(day: Day): string {
+    if(day === Day.Saturday || day === Day.Sunday) return "Weekend"
+    else{
+      return "Weekday"
+    }
+  }
   
   
-  
-  // problem ---> 8
+
+
+
+
+
   
   async function squareAsync(n: number): Promise<number> {
       return new Promise ((resolve, reject) => {
@@ -129,5 +141,3 @@ function formatString(input: string, toUpper?: boolean): string {
       })
   }
   
-  //squareAsync(4).then(console.log);        // Output after 1s: 16
-  //squareAsync(-3).catch(console.error);    // Output: Error: Negative number not allowed
